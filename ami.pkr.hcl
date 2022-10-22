0684f67a8b1d8c87e4c74766ef48143eb3807385
Shruti Sutrawe <77507762+shrutisutrawe@users.noreply.github.com>
2022-10-22 07:12:34 -0700

    Assignment4 (#16)
    * Assignment 4 changes to add EC2 instances and AMI
    
    * Assignment 4 changes to add EC2 instances and AMI
    
    * Assignment 4 changes to add EC2 instances and AMI
    
    * Assignment 4 changes to add EC2 instances and AMI
    
    * Assignment 4 changes to add EC2 instances and AMI
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 packer build github workflow changes
    
    * Assignment 4 generated war (artifacts) file
    
    * Assignment 4 generated war (artifacts) file
    
    * Assignment 4 generated war (artifacts) file
    
    * Assignment 4 generated war (artifacts) file
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 correcting war path
    
    * Assignment 4 deploy war file
    
    * Assignment 4 deploy war file
    
    * Assignment 4 add code to deploy spring boot application
    
    * Assignment 4 add code to deploy spring boot application
    
    * Assignment 4 add code to deploy spring boot application
    
    * Assignment 4 add code to deploy spring boot application
    
    * Assignment 4 add code to deploy spring boot application
    
    * Assignment 4 add code to deploy spring boot application

---
+++ ami.pkr.hcl
@@ -0,0 +1,80 @@
+variable "aws_region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "source_ami" {
+  type    = string
+  default = "ami-017fecd1353bcc96e" # Ubuntu 22.04 LTS
+}
+
+variable "ssh_username" {
+  type    = string
+  default = "ubuntu"
+}
+
+variable "subnet_id" {
+  type    = string
+  default = "subnet-0efede05bd423a4a7"
+}
+
+variable "profile" {
+  type    = string
+  default = "devuser"
+}
+
+variable "aws_demouser" {
+  type    = string
+  default = "097171053993"
+}
+
+variable "aws_devuser" {
+  type    = string
+  default = "958817607905"
+}
+
+# https://www.packer.io/plugins/builders/amazon/ebs
+source "amazon-ebs" "my-ami" {
+  region          = "${var.aws_region}"
+  profile         = "${var.profile}"
+  ami_name        = "csye6225_${formatdate("YYYY_MM_DD_hh_mm_ss", timestamp())}"
+  ami_description = "AMI for CSYE 6225"
+  ami_regions = [
+    "us-west-2",
+  ]
+  ami_users = [
+    "${var.aws_devuser}",
+    "${var.aws_demouser}",
+  ]
+
+  aws_polling {
+    delay_seconds = 120
+    max_attempts  = 50
+  }
+
+  instance_type = "t2.micro"
+  source_ami    = "${var.source_ami}"
+  ssh_username  = "${var.ssh_username}"
+  subnet_id     = "${var.subnet_id}"
+
+  launch_block_device_mappings {
+    delete_on_termination = true
+    device_name           = "/dev/sda1"
+    volume_size           = 50
+    volume_type           = "gp2"
+  }
+}
+
+build {
+  sources = ["source.amazon-ebs.my-ami"]
+
+  provisioner "shell" {
+    environment_vars = [
+      "DEBIAN_FRONTEND=noninteractive",
+      "CHECKPOINT_DISABLE=1"
+    ]
+    scripts = [
+      "scripts.sh"
+    ]
+  }
+}
---
+++ scripts.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+sudo apt-get clean
+sudo apt update
+sudo apt-get install openjdk-8-jdk -y
+sudo groupadd --system tomcat
+sudo useradd -d /usr/share/tomcat -r -s /bin/false -g tomcat tomcat
+export VER=10.0.27
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
+sudo tar xvf apache-tomcat-10.0.27.tar.gz -C /usr/share/
+sudo ln -s /usr/share/apache-tomcat-10.0.27/ /usr/share/tomcat
+sudo chown -R tomcat:tomcat /usr/share/tomcat
+sudo chown -R tomcat:tomcat /usr/share/apache-tomcat-10.0.27/
+echo "checking what is inside tomcat"
+echo -e '\n[Unit]\nDescription=Tomcat Server\nAfter=syslog.target network.target\n\n[Service]\nType=forking\nUser=tomcat\nGroup=tomcat\n\nEnvironment='JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64'\nEnvironment='JAVA_OPTS=-Djava.awt.headless=true'\nEnvironment='CATALINA_HOME=/usr/share/tomcat'\nEnvironment='CATALINA_BASE=/usr/share/tomcat'\nEnvironment='CATALINA_PID=/usr/share/tomcat/temp/tomcat.pid'\nEnvironment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'\n\nExecStart=/usr/share/tomcat/bin/startup.sh\nExecStop=/usr/share/tomcat/bin/shutdown.sh\n\n[Install]\nWantedBy=multi-user.target\n' | sudo tee /etc/systemd/system/tomcat.service
+sudo systemctl daemon-reload
+sudo systemctl start tomcat.service
+sudo systemctl enable tomcat.service
+sudo systemctl status tomcat.service
+ss -ltn
+sudo ufw allow from any to any port 8080 proto tcp
+
+sudo apt-cache search mysql-server
+sudo apt info -a mysql-server-8.0
+sudo apt-get install mysql-server-8.0 -y
+sudo systemctl is-enabled mysql.service
+sudo systemctl start mysql.service
+sudo systemctl status mysql.service
+export pwd=abc
+echo $pwd
+#export altercmd=\Alter user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Root@123'\
+#echo $altercmd
+sudo mysql -uroot -p$pwd --connect-expired-password -e "Alter user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Root@123'"
+sudo systemctl restart mysql.service
+sudo systemctl status mysql.service
+sudo mysql -uroot -pRoot@123 -e "CREATE DATABASE IF NOT EXISTS Csye6225WebServiceDB"
+
+sudo apt install maven -y
+
+sudo mkdir -p /home/ubuntu/logs
+sudo touch /home/ubuntu/logs/logs.out
+sudo chown ubuntu:ubuntu /home/ubuntu/logs/logs.out
+sleep 30
+sudo wget https://csye6225-shruti.s3.us-west-2.amazonaws.com/demo1-0.0.1-SNAPSHOT.jar
+nohup java -jar demo1-0.0.1-SNAPSHOT.jar 1> /home/ubuntu/logs/logs.out 2>&1 </dev/null &
